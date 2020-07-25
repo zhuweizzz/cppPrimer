154def57db364db9a999e9f57994526a815b352f
BayMax_z <1787973220@qq.com>
2020-07-25 23:35:38 +0800

    7-25

--- test.cpp
+++ test.cpp
@@ -5,29 +5,29 @@
 #include <typeinfo>
 #include <vector> 
 #include <iterator>
+#include <fstream>
+#include <sstream>
+#include <deque>
+#include <list>
+#include <array>
+
 
 using namespace std;
 
 
 int main()
 {
-    vector<int> nums = {1,2,3,4,5,6,7};
-    int k=3;
-    int index = 0;
-    int t;
-    int temp = nums[index];
-    if((!nums.empty()) && k!=0)
-    {
-        do
-        {
-            index += k;
-            while(index > (nums.size()-1))
-            {
-                index = index - nums.size();
-            }
-            t = nums[index];
-            nums[index]  = temp;
-            temp = t;
-        }while(index!=0);
-    }
+    vector<string> v1;
+    string s;
+
+    list<char *> ls{"testchar"};
+
+    v1.assign(ls.begin(),ls.end());
+
+    for(auto const &t : v1)
+        cout << t << endl;
+
+    system("pause");
+
+    return 0;
 } 
